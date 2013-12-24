55bc9fea3364851a64f9c2b446e4c2649fd85b1b
Ning <ning.m.zhang@gmail.com>
2013-12-24 16:33:00 +0800

    1st working version

--- yaws-based/src/appmod_coinproxy.erl
+++ yaws-based/src/appmod_coinproxy.erl
@@ -21,7 +21,7 @@ out(Arg) ->
     A2 = Arg#arg{state = [{ip, IP}]},
     yaws_rpc:handler_session(A2, {?MODULE, bitcoin_rpc_handler}).
 
-bitcoin_rpc_handler([{ip, IP}] = _State, {call, RPCMethod, Value} = _Request, _Session) ->
+bitcoin_rpc_handler([{ip, IP}] = _State, {call, RPCMethod, Value} = _RPCRequest, _Session) ->
     {127,0,0,1} = IP,
     io:format("Client IP: ~p~n", [IP]),
     io:format("RPC Method: ~p~n", [RPCMethod]),
@@ -31,13 +31,23 @@ bitcoin_rpc_handler([{ip, IP}] = _State, {call, RPCMethod, Value} = _Request, _S
     Method      = post,
     URL         = lists:append(["http://", ?RPC_USERNAME, ":", ?RPC_PASSWORD, "@", ?BITCOIND_HOST, ":", ?RPC_PORT, "/"]),
     ContentType = "text/json",
-    RequestBody = "{\"jsonrpc\": \"2.0\", \"id\":\"1\", \"method\": \"" ++ atom_to_list(RPCMethod) ++ "\", \"params\": []}",
+    %% RequestBody = "{\"jsonrpc\": \"2.0\", \"id\":\"1\", \"method\": \"" ++ atom_to_list(RPCMethod) ++ "\", \"params\": []}",
+    io:format("Args: ~p~n", [Args]),
+    RequestBody = ejson:encode({[{<<"method">>, atom_to_binary(RPCMethod, utf8)},
+				 {<<"params">>, lists:map(fun(X) -> case is_list(X) of true -> list_to_binary(X); false -> X  end end, Args)}]}),
     Request     = {URL, [], ContentType, RequestBody},
 
     {ok, Result} = httpc:request(Method, Request, [], []),
     {_Status, _Headers, Body} = Result,
 
-    {ok, {struct, [{"result", Response} | _]}} = json2:decode_string(Body),
-    io:format("Response from remote bitcoind host: ~p~n", [Response]),
-    
-    {true, 0, _Session, {response, Response}}.
+    {ok, {struct, [{"result", Response}, {"error", Error} | _]}} = json2:decode_string(Body),
+    io:format("Response: ~p~n", [Response]),
+    io:format("Response body: ~p~n", [Body]),
+    %% io:format("Response from remote bitcoind host: ~p~n", [Response]),
+
+    case Response of
+	null ->
+	    {true, 0, _Session, {response, Error}};
+	_ ->
+	    {true, 0, _Session, {response, Response}}
+    end.
