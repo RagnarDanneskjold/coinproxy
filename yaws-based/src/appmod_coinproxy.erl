a71d974f14fe2ea3d0219656d42fa36671848972
Ning <ning.m.zhang@gmail.com>
2013-12-24 09:05:53 +0800

    ""

--- yaws-based/src/appmod_coinproxy.erl
+++ yaws-based/src/appmod_coinproxy.erl
@@ -21,17 +21,6 @@ out(Arg) ->
     A2 = Arg#arg{state = [{ip, IP}]},
     yaws_rpc:handler_session(A2, {?MODULE, bitcoin_rpc_handler}).
 
-bitcoin_rpc_handler([{ip, IP}] = _State, {call, getinfo, _Value} = _Request, _Session) ->
-    io:format("Request = ~p, IP = ~p~n", [_Request, IP]),
-
-    %% lab
-    {ok, {{_, 200, _}, _, ResultStream}} = httpc:request(post, {"http://" ++ ?Username ++ ":" ++ ?Password ++ "@" ++ ?BitcoindHost ++ ":8332/", [], "text/json", "{\"jsonrpc\": \"2.0\", \"id\":\"1\", \"method\": \"getinfo\", \"params\": []}"}, [], []),
-
-    {ok, {struct, [{"result", Response} | _]}} = json2:decode_string(ResultStream),
-    io:format("~p~n", [Response]),
-    
-    {true, 0, _Session, {response, Response}};
-
 bitcoin_rpc_handler([{ip, IP}] = _State, {call, Method, _Value} = _Request, _Session) ->
     io:format("Request = ~p, IP = ~p~n", [_Request, IP]),
 
@@ -41,10 +30,10 @@ bitcoin_rpc_handler([{ip, IP}] = _State, {call, Method, _Value} = _Request, _Ses
     {ok, {struct, [{"result", Response} | _]}} = json2:decode_string(ResultStream),
     io:format("~p~n", [Response]),
     
-    {true, 0, _Session, {response, Response}};
+    {true, 0, _Session, {response, Response}}.
 
-bitcoin_rpc_handler([{ip, IP}] = _State, {call, test, _Value} = _Request, _Session) ->
-    io:format("Request = ~p, IP = ~p~n", [_Request, IP]),
-    %% error: {"code":-32601,"message":"Method not found"}
-    Response = {struct, [{"code", -32601}, {"message", "Method not found"}]},
-    {false, {error, Response}}.
+%% bitcoin_rpc_handler([{ip, IP}] = _State, {call, test, _Value} = _Request, _Session) ->
+%%     io:format("Request = ~p, IP = ~p~n", [_Request, IP]),
+%%     %% error: {"code":-32601,"message":"Method not found"}
+%%     Response = {struct, [{"code", -32601}, {"message", "Method not found"}]},
+%%     {false, {error, Response}}.
