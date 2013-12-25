647a1be1a54718af4ee3e1dcd0ff300f60cb9cdb
Ning <ning.m.zhang@gmail.com>
2013-12-25 13:45:31 +0800

    1st sinatra-based working version

--- sinatra-based/coinproxy.rb
+++ sinatra-based/coinproxy.rb
@@ -1,12 +1,32 @@
 require 'sinatra'
 require 'json'
+require 'net/http'
+
+# require './conf.rb'
+require './conf_dev.rb'
 
 set :port, 8332
 
 post '/' do
+  # Unpack the request
   request.body.rewind
   data = JSON.parse request.body.read
   puts "Client IP: #{request.ip}"
   puts "RPC Method: #{data['method']}"
   puts "RPC Arguments: #{data['params']}"
+
+  # Repack and forward the request
+  @post_ws = "/"
+  @payload = {"method" => data['method'], "params" => data['params']}.to_json
+
+  req = Net::HTTP::Post.new(@post_ws, initheader = {'Content-Type' => 'application/json'})
+  req.basic_auth settings.rpcusername, settings.rpcpassword
+  req.body = @payload
+
+  # Accept the response from bitcoind
+  res = Net::HTTP.new(settings.host, settings.port).start {|http| http.request(req)}
+  puts "#{res.code} #{res.message} #{res.body}"
+  
+  # Forward the response to the original client
+  res.body
 end
---
+++ sinatra-based/conf.rb
@@ -0,0 +1,4 @@
+set :rpcusername, "username"
+set :rpcpassword, "password"
+set :host, "example.com"
+set :port, "8332"
