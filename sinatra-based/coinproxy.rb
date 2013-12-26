1976aacf3b6ea90fcd0cd725d45540a50ccd593c
Ning <ning.m.zhang@gmail.com>
2013-12-26 14:38:21 +0800

    ""

--- sinatra-based/coinproxy.rb
+++ sinatra-based/coinproxy.rb
@@ -15,18 +15,31 @@ post '/' do
   puts "RPC Method: #{data['method']}"
   puts "RPC Arguments: #{data['params']}"
 
-  # Repack and forward the request
-  @post_ws = "/"
-  @payload = {"method" => data['method'], "params" => data['params']}.to_json
+  api_list = JSON.parse(IO.read("./api_controller.json"))
 
-  req = Net::HTTP::Post.new(@post_ws, initheader = {'Content-Type' => 'application/json'})
-  req.basic_auth settings.rpcusername, settings.rpcpassword
-  req.body = @payload
+  case api_list[data['method']]
+  when nil
+    error = "This API is not defined!"
+    puts error
+    {"result" => "null", "error" => error, "id" => "null"}.to_json.dump
+  when "enabled"
+    # Repack and forward the request
+    @post_ws = "/"
+    @payload = {"method" => data['method'], "params" => data['params']}.to_json
 
-  # Accept the response from bitcoind
-  res = Net::HTTP.new(settings.host, settings.port).start {|http| http.request(req)}
-  puts "#{res.code} #{res.message} #{res.body}"
-  
-  # Forward the response to the original client
-  res.body
+    req = Net::HTTP::Post.new(@post_ws, initheader = {'Content-Type' => 'application/json'})
+    req.basic_auth settings.rpcusername, settings.rpcpassword
+    req.body = @payload
+
+    # Accept the response from bitcoind
+    res = Net::HTTP.new(settings.host, settings.port).start {|http| http.request(req)}
+    puts "#{res.code} #{res.message} #{res.body}"
+    
+    # Forward the response to the original client
+    res.body
+  else
+    error =  "This API is not enabled!"
+    puts error
+    {"error" => error}.to_json.dump
+  end
 end
